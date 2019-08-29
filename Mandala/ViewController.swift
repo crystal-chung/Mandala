0ab78d665382428294982303cc23b58deb5c3744
Crystal Chung <crystal_chung@intuit.com>
2019-08-29 10:26:06 -0700

    Initial Commit

---
+++ Mandala/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mandala
+//
+//  Created by Chung, Crystal on 8/29/19.
+//  Copyright © 2019 Chung, Crystal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
